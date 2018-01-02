1f579f4ea9b7d68f37ac51f34570d8bca9d7a57e
wjbluo <1115419861@qq.com>
2018-01-02 10:24:47 +0800

    跑滑行道日志新增

--- runwayPatroManagement/views.py
+++ runwayPatroManagement/views.py
@@ -7,18 +7,36 @@ from evaluationManagement.models import parameterTab
 # Create your views here.
 
 def showPatrolRecord(request):
-
-    return render(request, 'runwaysPatroManagement/runways/runwayPatroRecord.html')
+    currentDate = datetime.datetime.now().strftime("%Y%m%d")
+    context={
+        'currentDate':currentDate
+    }
+    return render(request, 'runwaysPatroManagement/runways/runwayPatroRecord.html',context)
 
 
 def addPatrolRecord(request):
     patroDate = datetime.datetime.now().strftime("%Y%m%d")
     allrunways = runway_baseData.objects.all()
     patrolType = parameterTab.objects.filter(parameterType='patrolType')
-    runwaysPatroData.objects.filter()
+    patroData = runwaysPatroData.objects.filter(patroDate__contains=patroDate)
+    tzPatroCount = patroData.filter(patroCount='05').count()+1
+    patrolTypeData = patrolTypeDic()
+    for type in patrolTypeData:
+        for item in patroData:
+            if item.patroCount==type['code']:
+                type['code'] = 'false'
     context={
         'patroDate':patroDate,
         'allrunways':allrunways,
-        'patrolType':patrolType
+        'patrolTypeData':patrolTypeData,
+        'tzPatroCount':tzPatroCount
     }
     return render(request, 'runwaysPatroManagement/runways/addPatrol_Dialog.html',context)
+
+def patrolTypeDic():
+    patrolType = parameterTab.objects.filter(parameterType='patrolType')
+    patrolTypeData = []
+    for item in patrolType:
+        dict = {'code':item.parameterCode,'name':item.parameterName,'flag':'true'}
+        patrolTypeData.append(dict)
+    return patrolTypeData
